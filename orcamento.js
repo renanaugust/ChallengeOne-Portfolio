a32f91ee7ba573147e8dfd264c43146bccaecc12
Renan Augusto <43729151+renanaugust@users.noreply.github.com>
2023-03-02 13:24:44 -0300

    adicionando formulário de orçamento

---
+++ orcamento.js
@@ -0,0 +1,24 @@
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#cb1-6").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#cb1-6").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+  
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+   
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
